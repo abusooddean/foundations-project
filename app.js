e4d284921b4c9c57d97fae63b6c2c378dd321ea9
Dean <190409058+abusooddean@users.noreply.github.com>
2025-09-08 16:09:13 -0400

    Set up user routes and refactor

--- app.js
+++ app.js
@@ -1,22 +1,17 @@
 const {logger} = require('./utils/logger');
+const userRoutes = require('./routes/userRoutes')
 const express = require('express')
+
 const app = express()
 const port = 3000
 
-//log in for both employee and financial manager
-//need to have auth access for each regarding requests
+app.use(express.json())
 
-//employee: login, add new or view past requests
-//manager: login, view all requests + pending + filter option, approve or deny requests (update employee request status)
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-  logger.log({
-  level: 'info',
-  message: 'Test'
-});
-})
+app.use("/user", userRoutes)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
---
+++ routes/userRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const router = express.Router();
+
+//TO DO:
+// Login / Register Feature
+// The login and register feature is meant to give you preliminary experience handling authentication within an application. 
+// These features allow you to ensure you can track or maintain who can connect to your application. 
+// User Stories As an Employee or Manager, I should be able to log into the application.
+
+router.post("/register", async (req, res) => {
+    //register with username and password
+    //username must be unique
+    //set default employee role, maybe in model?
+})
+
+router.post("/login", async (req, res) => {
+    //first must check if username exists
+    //then login user if password matches
+})
+
+module.exports = router;
