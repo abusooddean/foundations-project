b14f039f56edba541c9c9f526520396a158aa042
Dean <190409058+abusooddean@users.noreply.github.com>
2025-09-10 16:23:27 -0400

    Update createTicket to have user_id field

--- src/service/ticketService.js
+++ src/service/ticketService.js
@@ -2,10 +2,11 @@ const ticketDAO = require("../repository/ticketDAO");
 const {logger} = require('../util/logger')
 
 async function createTicket(ticket){
-    //check before creating
+    //check fields before creating
     if(validateTicket(ticket)){
         const data = await ticketDAO.createTicket({
             ticket_id: crypto.randomUUID(),
+            user_id: ticket.user_id,
             amount: ticket.amount,
             description: ticket.description,
             status: "Pending",
@@ -18,10 +19,32 @@ async function createTicket(ticket){
     }
 }
 
+// async function updateTicket(ticket){  --- only need certain values such as ticket_id ? and update its status ---
+//     if(ticket){
+//     const data = await ticketDAO.updateTicket({
+//             ticket_id: ticket.ticket_id,
+//             user_id: ticket.user_id,
+//             amount: ticket.amount,
+//             description: ticket.description,
+//             status: ticket.status,
+//         })
+//         logger.info(`Updating ticket: ${JSON.stringify(data)}`);
+//         return data;
+//     }else{
+//         logger.info(`Failed to update ticket: ${JSON.stringify(ticket)}`);
+//         return null;
+//     }
+// }
+
 function validateTicket(ticket){
     const ticketAmount = ticket.amount > 0;
     const ticketDescription = ticket.description.length > 0;
     return (ticketAmount && ticketDescription);
 }
 
-createTicket({amount: 10 , description: ""});
+//TESTING
+// createTicket({user_id: "1", amount: 10 , description: "business dinner"});
+// createTicket({user_id: "be1e0bc1-9608-4ce4-887d-bcec6c5ced8a", amount: 100 , description: "flight"});
+// createTicket({user_id: "be1e0bc1-9608-4ce4-887d-bcec6c5ced8a", amount: 1000 , description: "dinner"});
+
+// updateTicket({ticket_id: "933fdc36-9512-454e-8eb5-a035819ce859", user_id: "be1e0bc1-9608-4ce4-887d-bcec6c5ced8a", amount: 10 , description: "business lunch", status:"Processing"});
