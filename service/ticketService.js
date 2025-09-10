e5831697616fa0cc6f2d0abfd7786324eef0ead9
Dean <190409058+abusooddean@users.noreply.github.com>
2025-09-10 14:06:07 -0400

    Moved files into src

--- service/ticketService.js
+++
@@ -1,9 +0,0 @@
-const ticketDAO = require("../repository/ticketDAO");
-const {checkIfValid} = require("../utils/verification");
-
-async function createTicket(amount, description, status= "Pending"){
-    const result = await ticketDAO.createTicket({ticket_id: crypto.randomUUID(), amount, description, status});
-    console.log(checkIfValid(result, "create", "ticket"))
-}
-
-createTicket(100, "business lunch");
